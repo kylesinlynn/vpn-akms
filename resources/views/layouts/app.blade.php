beb0fddbdf28959545287e2ac70b3e7c8e0d57e3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2021-02-26 18:08:22 +0630

    Place The UI

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta title="title" content="MCY - VPN Server List" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="MCY - VPN Active Server List. Outline VPN နှင့်အသုံးပြုရန် VPN Server List ဖြစ်ပါတယ်။" />
+    <meta name="keywords" content="Myanmar,VPN,MCY Asia,MCY, Myanmar Cyber Youth,မြန်မာ,VPN List">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="1 days">
+    <meta name="author" content="Aung Myat Moe (@amm834)">
+
+    <title>MCY - VPN Server List</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+</head>
+
+<body class="bg-gray-50">
+    
+@yield('content')
+
+@include('layouts.footer')
+@stack('scripts')
+</body>
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+    <footer class="text-center py-5 bg-green-600 text-white select-none">
+        &copy; 2021-2022 By MCY Team.
+    </footer>
