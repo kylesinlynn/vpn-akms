a4479242b1e4e672b91bb1d83f7239703210c546
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-23 04:20:13 +0630

    feat: add vue

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    Hey
+  </main>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
-use App\Models\Server;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PageController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ServerController;
 
-Route::get('/', function () {
-    $servers = Server::orderByDesc('id')->get();
-    return view('index',compact('servers'));
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/{any?}', function () {
+    return view('main');
 });
 
-
-Route::resource('/register',UserController::class);
-Route::resource('/page',PageController::class);
-Route::get('/login',[LoginController::class,'index'])->name('login.index');
-Route::post('/login',[LoginController::class,'login'])->name('login');
-
-Route::resource('/server',ServerController::class);
