056f203935727ef09922ed505cd3febf7efc1887
Kyle Sin Lynn <kylesinlynn@gmail.com>
2022-09-29 22:02:52 +0630

    fix: auth api routes"

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+// authentication
+Route::prefix('auth')->group(function () {
+    Route::post('register', [RegisterController::class, 'action'])->name('auth.register');
+    Route::post('login', [LoginController::class, 'action'])->name('auth.login');
+    Route::get('logout', [LogoutController::class, 'action'])->name('auth.logout');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{route?}', function () {
     return view('main');
 });
 
-// authentication
-Route::prefix('auth')->group(function () {
-    Route::post('register', [RegisterController::class, 'action'])->name('auth.register');
-    Route::post('login', [LoginController::class, 'action'])->name('auth.login');
-    Route::post('logout', [LogoutController::class, 'action'])->name('auth.logout');
-});
